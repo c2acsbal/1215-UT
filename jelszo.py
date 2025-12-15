8b4a6d80e8a3f64c2f9b2e9149640edf3d908d74
c2acsbal <c2acsbal@vasvari.org>
2025-12-15 09:22:09 +0100

    jelszó feladat elkészítése

---
+++ jelszo.py
@@ -0,0 +1,27 @@
+def jelsz_erosseg(jelszo):
+    erosseg = 1
+    if len(jelszo) >= 5:
+        erosseg += 1
+
+
+    if len(jelszo) >= 8:
+        erosseg += 2
+
+    spec_karakter = ["_", "-", "."]
+
+    for i in jelszo:
+        if i in spec_karakter:
+            erosseg += 2
+        
+    
+    if "jelszo" in jelszo or "123" in jelszo:
+        erosseg = 0
+    
+    if len(jelszo) == 0:
+        erosseg = 0
+    
+    return erosseg
+
+jelszo = input("Input: ")
+
+print(jelsz_erosseg(jelszo))
